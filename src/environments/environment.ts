df7338170423166769686c4b6002d6c3ef70f761
Suprem Trivedi <suprem.trivedi@FRI0064.local>
2018-10-02 15:14:33 +0100

    First commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,22 @@
+export const environment = {
+    production: false,
+
+    region: 'us-east-2',
+
+    identityPoolId: 'us-east-2:7e1a4304-dd69-44fd-88a4-6a8c3388b7dd',
+    userPoolId: 'us-east-2_LMkZSJrOZ',
+    clientId: 'misaq2fi4uanmnt8fvfimcb1b',
+
+    rekognitionBucket: 'rekognition-pics',
+    albumName: "usercontent",
+    bucketRegion: 'us-east-2',
+
+    ddbTableName: 'LoginTrailangularpoc',
+
+    cognito_idp_endpoint: '',
+    cognito_identity_endpoint: '',
+    sts_endpoint: '',
+    dynamodb_endpoint: '',
+    s3_endpoint: ''
+};
+
